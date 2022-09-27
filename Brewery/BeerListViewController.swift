f4ce78a5275dc1b2b0989a0db384c21fb1bbb24c
RaeHoon Baek <bmp117@naver.com>
2022-09-27 17:00:52 +0900

    Brewery 예제 - 스토리보드가 아닌 코드 작업을 위한 info.plist 항목 제거 작업 (https://zhiblog.tistory.com/15)

--- Brewery/BeerListViewController.swift
+++ Brewery/BeerListViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class BeerListViewController: UITableViewController {
     
     var beerList = [Beer]()
+    var currentPage = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +24,8 @@ class BeerListViewController: UITableViewController {
         tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
         tableView.rowHeight = 150
         
+        fetchBeer(of: currentPage)
+        
     }
 }
 
@@ -33,9 +36,9 @@ extension BeerListViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as! BeerListCell
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as! BeerListCell
         
-//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as? BeerListCell else { return UITableViewCell() }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for: indexPath) as? BeerListCell else { return UITableViewCell() }
         
         let beer = beerList[indexPath.row]
         cell.configure(with: beer)
@@ -52,4 +55,56 @@ extension BeerListViewController {
         self.show(detailViewController, sender: nil)
     }
     
+}
+
+//Data Fetching
+private extension BeerListViewController {
+    func fetchBeer(of page: Int) {
+        guard let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+            guard error == nil,
+                  let self = self,
+                  let response = response as? HTTPURLResponse,
+                  let data = data,
+                  let beers = try? JSONDecoder().decode([Beer].self, from: data) else {
+                print("ERROR: URLSession data task \(error?.localizedDescription ?? "")")
+                return
+            }
+            
+            switch response.statusCode {
+            case (200...299): //성공
+                self.beerList += beers
+                self.currentPage += 1
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
+            case (400...499): //클라이언트 에러
+                print("""
+                    ERROR: Client ERROR \(response.statusCode)
+                    Response: \(response)
+                """)
+                
+            case (500...599): //서버 에러
+                print("""
+                    ERROR: Server ERROR \(response.statusCode)
+                    Response: \(response)
+                """)
+            default:
+                print("""
+                    ERROR: \(response.statusCode)
+                    Response: \(response)
+                """)
+                
+            }
+        }
+        dataTask.resume()
+        
+        
+    }
 }

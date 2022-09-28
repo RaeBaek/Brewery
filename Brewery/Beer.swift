b4374967e7d96f51de9455efff6615abac58dcc8
RaeHoon Baek <bmp117@naver.com>
2022-09-28 16:07:33 +0900

    Brewery 예제 - 맥주리스트 가져오기

--- Brewery/Beer.swift
+++ Brewery/Beer.swift
@@ -11,7 +11,7 @@ import Foundation
 struct Beer: Decodable {
     let id: Int?
     let name, taglineString, description, brewersTips, imageURL: String?
-    let foodParing: [String]?
+    let foodPairing: [String]?
     
     var tagLine: String {
         let tags = taglineString?.components(separatedBy: ". ")
@@ -27,7 +27,7 @@ struct Beer: Decodable {
         case taglineString = "tagline"
         case imageURL = "image_url"
         case brewersTips = "brewers_tips"
-        case foodParing = "food_pairing"
+        case foodPairing = "food_pairing"
     }
 }
 
